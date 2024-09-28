b2e1af8afc9063c426ec713c0cfe66234c1dd189
speedsterkawaii <avtarvirdi82@gmail.com>
2024-09-28 00:14:21 -0400

    Update and rename init.luau to init.lua

---
+++ storage/init.lua
@@ -0,0 +1,10 @@
+-- saturn script init
+-- todo: add more 
+-- note: most of these are fake i realized giving an actual init scirpt is not a good idea
+-- dont modify this really idrc
+
+-- create a storage folder inside of game->CoreGui which will hold our modulescripts we will later edit them on in the c part
+local junk = Instance.new("Folder") -- create folder
+junk.Parent = game.CoreGui -- set parent to coregui
+junk.Name = "SATURN" -- name will be saturn for this case
+
